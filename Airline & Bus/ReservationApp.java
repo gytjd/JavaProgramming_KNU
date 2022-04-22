93c73aeed35e57f14001bdbfab2ac68b19ec8ef1
Blue <101933437+gytjd@users.noreply.github.com>
2022-04-22 15:50:30 +0900

    Add files via upload
    Bus & Airline reservation system 수정 1

--- Airline & Bus/ReservationApp.java
+++ Airline & Bus/ReservationApp.java
@@ -3,220 +3,183 @@ package 과제2;
 import java.util.Scanner;
 
 
+
+
 public class ReservationApp {
-	
 	
 	public void print_menu()
 	{
 		Scanner scanner=new Scanner(System.in);
-		
-		
-		AirlineSystem airline=new AirlineSystem(scanner); // 화면 입력을 위해 생성한 Scanner 객체는 AirlineSystem 클래스와 BusSystem 클래스의 생성자의 파라미터로 전달하여 하나의 Scanner 객체를 공유함
-		
-		airline.makeSeats(4, 10);
-		
-		
-		BusSystem bus=new BusSystem(scanner);
-		bus.makeSeats(3, 10); // “0번 통합 예약 시스템 종료” 메뉴를 선택하기 전까지는 기존 예약된 현황을 유지하고 있어야 됨
-		
-		
+		int menuNum;
 		
 		while(true)
 		{
-			String menuNum;
-			int menuNumber;
-			char menuchar;
+			System.out.println("------------------------------------");
+			System.out.println("K-Startup의 통합 예약 시스템을 방문해 주셔서 감사합니다.");
+			System.out.println("  1. 항공사 예약 시스템");
+			System.out.println("  2. 버스 예약 시스템");
+			System.out.println("  0. 통합 예약 시스템 종료");
+			System.out.println("------------------------------------");
 			
-			System.out.println("--------------------------------------------"); // 메인 메뉴를 화면에 출력하고, 사용자 입력 처리(오류 처리 기능 추가 )
-			System.out.println("K-Startup의 통합 예약 시스템을 방문해 주서 감사합니다.");
-			System.out.println("   1. 항공사 예약 시스템");
-			System.out.println("   2. 버스 예약 시스템");
-			System.out.println("   0. 통합 예약 시스템 종료");
-			System.out.println("--------------------------------------------");
-			System.out.print("메뉴를 선택해주세요 : ");
+		try {
+			System.out.print("메뉴를 선택해주세요: ");
+			menuNum=scanner.nextInt();
 			
-			menuNum=scanner.next();
-			menuNumber=menuNum.charAt(0)-'0'; // 사용자의 선택에 따라 항공사 예약 시스템 또는 버스 예약 시스템 기능을 실행시킴
-			menuchar=menuNum.charAt(0);
-	
-			if(menuNumber==1)
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("잘못된 메뉴 선택입니다. 다시 입력해주세요 (0~2)");
+			scanner.nextLine();
+			continue;
+		}
+			
+			if(menuNum==1)
 			{
-				int n;
+				AirlineSystem airlineSystem=new AirlineSystem(scanner);
+				airlineSystem.makeSeats(4, 10);
+				
+				
 				while(true)
 				{
-					System.out.println("-------------------");
-					System.out.println("항공사 예약 시스템");
+					System.out.println("--------------------");
+					System.out.println("항공기 예약 시스템");
 					System.out.println("1. 좌석 예약");
 					System.out.println("2. 예약 취소");
 					System.out.println("3. 예약 현황 조회");
 					System.out.println("4. 메인 메뉴로 돌아가기");
-					System.out.println("-------------------");
-					System.out.print("번호를 선택해주세요 : ");
+					System.out.println("--------------------");
 					
-					airline.set_airlineNum();
-					airline.set_airlen();
-					airline.set_airlineNumber();
-					airline.set_aritlineChar();
 					
-					if(airline.airlen==1)
+					try {
+						System.out.print("번호를 선택해주세요: ");
+						airlineSystem.set_airlineMenunum();
+						
+					} catch (Exception e) {
+						// TODO: handle exception
+						
+						System.out.println("잘못된 메뉴 선택입니다. 숫자를 다시 입력해주세요 (1~4)");
+						scanner.nextLine();
+						continue;
+					}
+					
+					if(airlineSystem.airlineMenunum==1)
 					{
-						if(airline.airlineNumber==1)
+						System.out.print("예약할 좌석 이를을 입력하세요: ");
+						airlineSystem.set_airlineSeat();
+						if(airlineSystem.reserveSeat(airlineSystem.airlineSeat)==1)
 						{
-							System.out.print("예약할 좌석 이름을 입력하세요 : ");
-							airline.set_airlineReserve();
-							
-							if(airline.reserveSeat(airline.airlineReserve)==0)
-							{
-								airline.displaySeat("항공사 예약 시스템");
-							}
+							airlineSystem.displaySeat("항공기 예약 시스템");
 						}
-						else if(airline.airlineNumber==2)
+					}
+					else if(airlineSystem.airlineMenunum==2)
+					{
+						System.out.print("취소할 좌석 이를을 입력하세요: ");
+						airlineSystem.set_airlineSeat();
+						if(airlineSystem.cancelSeat(airlineSystem.airlineSeat)==1)
 						{
-							System.out.print("취소할 좌석 이름을 입력하세요 : ");
-							airline.set_airlineCancel();
-							if(airline.cancelSeat(airline.airlineCancel)==0)
-							{
-								airline.displaySeat("항공사 예약 시스템");
-							}
+							airlineSystem.displaySeat("항공기 예약 시스템");
 						}
-						else if(airline.airlineNumber==3)
-						{
-							airline.displaySeat("항공사 예약 시스템");
-						}
-						else if(airline.airlineNumber==4)
-						{
-							System.out.println("항공사 예약 시스템을 종료하고 메인 메뉴로 돌아갑니다.");
-							break;
-						}
-						else 
-						{
-							if((airline.airlineChar>='a' && airline.airlineChar<='z')||(airline.airlineChar>='A'&& airline.airlineChar<='Z'))
-							{
-								System.out.println("잘못된 메뉴 선택입니다.숫자를 다시 입력하세요(1~4).");
-							}
-							else
-							{
-								System.out.println("잘못된 메뉴 선택입니다. 다시 입력하세요(1~4).");
-							}
-						}
+					}
+					else if(airlineSystem.airlineMenunum==3)
+					{
+						airlineSystem.displaySeat("항공기 예약 시스템");
+					}
+					else if(airlineSystem.airlineMenunum==4)
+					{
+						System.out.println("항공기 예약 시스템을 종료하고 메인 메뉴로 돌아갑니다.");
+						break;
 					}
 					else
 					{
-						if((airline.airlineChar>='a' && airline.airlineChar<='z')||(airline.airlineChar>='A'&& airline.airlineChar<='Z'))
-						{
-							System.out.println("잘못된 메뉴 선택입니다.숫자를 다시 입력하세요(1~4).");
-						}
-						else
-						{
-							System.out.println("잘못된 메뉴 선택입니다. 다시 입력하세요(1~4).");
-						}
+						System.out.println("잘못된 메뉴 선택입니다. 다시 입력하세요(1~4).");
 					}
 				}
-			
 			}
-			else if(menuNumber==2)
+			else if(menuNum==2)
 			{
+				BusSystem busSystem=new BusSystem(scanner);
 				
+				busSystem.makeSeats(3, 10);
+				
+				int busNum;
 				
 				while(true)
 				{
-					System.out.println("-------------------");
+					System.out.println("--------------------");
 					System.out.println("버스 예약 시스템");
 					System.out.println("1. 좌석 예약");
 					System.out.println("2. 예약 취소");
 					System.out.println("3. 예약 현황 조회");
 					System.out.println("4. 메인 메뉴로 돌아가기");
-					System.out.println("-------------------");
-					System.out.print("번호를 선택해주세요 : ");
-					bus.set_busNum();
-					bus.set_buslen();
-					bus.set_busNumber();
-					bus.set_busChar();
-
+					System.out.println("--------------------");
 					
-					if(bus.buslen==1)
+					
+					try {
+						System.out.print("번호를 선택해주세요: ");
+						busSystem.set_busMenunum();
+						
+					} catch (Exception e) {
+						// TODO: handle exception
+						
+						System.out.println("잘못된 메뉴 선택입니다. 숫자를 다시 입력해주세요 (1~4)");
+						scanner.nextLine();
+						continue;
+					}
+					
+					if(busSystem.busMenunum==1)
 					{
-						if(bus.busNumber==1)
+						System.out.print("예약할 좌석 이름을 입력하세요: ");
+						busSystem.set_busSeat();
+						if(busSystem.reserveSeat(busSystem.busSeat)==1)
 						{
-							
-							System.out.print("예약할 좌석 이름을 입력하세요 : ");
-							bus.set_busReserve();
-							
-							if(bus.reserveSeat(bus.busReserve)==0)
-							{
-								bus.displaySeat("버스 예약 시스템");
-							}
-							
-							
+							busSystem.displaySeat("버스 예약 시스템");
 						}
-						else if(bus.busNumber==2)
+					}
+					else if(busSystem.busMenunum==2)
+					{
+						System.out.print("취소할 좌석 이름을 입력하세요: ");
+						busSystem.set_busSeat();
+						
+						if(busSystem.cancelSeat(busSystem.busSeat)==1)
 						{
-							System.out.print("취소할 좌석 이름을 입력하세요 : ");
-						    bus.set_busCancel();
-						    
-							if(bus.cancelSeat(bus.busCancel)==0)
-							{
-								bus.displaySeat("버스 예약 시스템");
-							}
-							
+							busSystem.displaySeat("버스 예약 시스템");
 						}
-						else if(bus.busNumber==3)
-						{
-							bus.displaySeat("버스 예약 시스템");
-						}
-						else if(bus.busNumber==4)
-						{
-							System.out.println("버스 예약 시스템을 종료하고 메인 메뉴로 돌아갑니다.");
-							break;
-						}
-						else 
-						{
-							if((bus.busChar>='a' && bus.busChar<='z')||(bus.busChar>='A'&& bus.busChar<='Z'))
-							{
-								System.out.println("잘못된 메뉴 선택입니다.숫자를 다시 입력하세요(1~4).");
-							}
-							else
-							{
-								System.out.println("잘못된 메뉴 선택입니다. 다시 입력하세요(1~4).");
-							}
-						}
+					}
+					else if(busSystem.busMenunum==3)
+					{
+						busSystem.displaySeat("버스 예약 시스템");
+					}
+					else if(busSystem.busMenunum==4)
+					{
+						System.out.println("버스 예약 시스템을 종료하고 메인 메뉴로 돌아갑니다.");
+						break;
 					}
 					else
 					{
-						if((bus.busChar>='a' && bus.busChar<='z')||(bus.busChar>='A'&& bus.busChar<='Z'))
-						{
-							System.out.println("잘못된 메뉴 선택입니다.숫자를 다시 입력하세요(1~4).");
-						}
-						else
-						{
-							System.out.println("잘못된 메뉴 선택입니다. 다시 입력하세요(1~4).");
-						}
+						System.out.println("잘못된 메뉴 선택입니다. 다시 입력하세요(1~4).");
 					}
 				}
+				
+				
+				
 			}
-			else if(menuNumber==0)
+			else if(menuNum==0)
 			{
 				System.out.println("K-Startup 통합 예약 시스템을 종료합니다.");
 				break;
 			}
-			else
-			{	
-				if((menuchar>='a' && menuchar<='z')||(menuchar>='A'&& menuchar<='Z'))
-				{
-					System.out.println("잘못된 메뉴 선택입니다.숫자를 다시 입력하세요(0~2).");
-				}
-				else
-				{
-					System.out.println("잘못된 메뉴 선택입니다. 다시 입력하세요(0~2).");
-				}
+			else 
+			{
+				System.out.println("잘못된 메뉴 선택입니다. 다시 입력해주세요 (0~2)");
 			}
+			
 		}
 	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		ReservationApp app=new ReservationApp();
-		app.print_menu();
+		ReservationApp reservationApp=new ReservationApp();
+		reservationApp.print_menu();
 	}
 
 }
